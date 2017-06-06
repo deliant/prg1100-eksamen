1625402014e2f71ce186dd9d36b62990ebfb5508
Marius Norheim <xeroxmf@gmail.com>
2017-06-06 13:36:57 +0200

    Lagt til pasient

--- vedlikehold/libs/listeboks.php
+++ vedlikehold/libs/listeboks.php
@@ -29,6 +29,21 @@ function listeboksBilde() {
   mysqli_close($conn);
 }
 
+function listeboksPasient() {
+  include("db.php");
+  $sql = "SELECT personnr FROM pasient";
+  $result = mysqli_query($conn, $sql);
+
+  if(mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+      echo "<option value=". $row['personnr'] .">". $row['personnr'] ."</option>\n";
+    }
+  } else {
+    echo "<option value=\"Ingen\">Ingen pasienter funnet</option>\n";
+  }
+  mysqli_close($conn);
+}
+
 function listeboksTimeinndeling() {
   include("db.php");
   $brukernavn = mysqli_real_escape_string($conn, $_POST["velgTimeinndelingBehandler"]);
