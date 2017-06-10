218f590a87cfdcdbe706037246d38a90c7187c21
Marius Norheim <xeroxmf@gmail.com>
2017-06-10 21:30:57 +0200

    Fjernet unødvendig funksjon

--- vedlikehold/libs/listeboks.php
+++ vedlikehold/libs/listeboks.php
@@ -46,42 +46,6 @@ function listeboksPasient() {
   mysqli_close($conn);
 }
 
-function listeboksTimebestilling() {
-  include("db.php");
-  $sql = "SELECT t.timebestillingnr, t.dato, t.brukernavn, t.personnr
-  FROM timebestilling AS t
-  LEFT JOIN behandler AS b ON t.brukernavn = b.brukernavn
-  LEFT JOIN pasient AS p ON t.personnr = p.personnr
-  ORDER BY t.dato";
-  $result = mysqli_query($conn, $sql);
-
-  if(mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-      echo "<option value=\"". htmlspecialchars($row['personnr']) ."\">". htmlspecialchars($row['personnr']) ." - ". htmlspecialchars($row['pasientnavn']) ."</option>\n";
-    }
-  } else {
-    echo "<option value=\"Ingen\">Ingen timebestillinger funnet</option>\n";
-  }
-  mysqli_close($conn);
-}
-
-function listeboksTimeinndeling() {
-  include("db.php");
-  $brukernavn = mysqli_real_escape_string($conn, $_POST["velgTimeinndelingBehandler"]);
-  $ukedag = mysqli_real_escape_string($conn, $_POST["velgTimeinndelingUkedag"]);
-  $sql = "SELECT * FROM timeinndeling WHERE brukernavn='$brukernavn' AND ukedag='$ukedag'";
-  $result = mysqli_query($conn, $sql);
-
-  if(mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-      echo "<option value=\"". htmlspecialchars($row['tidspunkt']) ."\">". htmlspecialchars($row['tidspunkt']) ."</option>\n";
-    }
-  } else {
-    echo "<option value=\"Ingen\">Ingen timeinndelinger funnet</option>\n";
-  }
-  mysqli_close($conn);
-}
-
 function listeboksYrkesgruppe() {
   include("db.php");
   $sql = "SELECT yrkesgruppenr, yrkesgruppenavn FROM yrkesgruppe ORDER BY yrkesgruppenavn";
