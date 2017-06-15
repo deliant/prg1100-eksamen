ee5bf3a0f4cadb21aea7cda5e2ae52979d11ab01
Marius Norheim <xeroxmf@gmail.com>
2017-06-15 06:39:38 +0200

    Lagt til datepicker

---
+++ vedlikehold/js/datepicker.js
@@ -0,0 +1,30 @@
+function initDatepicker(elem) {
+  $(elem).datepicker({
+    dateFormat: 'yy-mm-dd',
+    prevText:'',
+    nextText:'',
+    minDate: "0",
+    maxDate: "+365"
+  });
+}
+
+function initTimepicker(elem) {
+  $(elem).timepicker({
+    timeFormat: 'HH:mm',
+    interval: '15',
+    minTime: '08',
+    maxTime: '16',
+    startTime: '08:00',
+    dynamic: true,
+    dropdown: true,
+    scrollbar: true
+  });
+}
+
+$(document).ready(function(){
+  initDatepicker('#regDato, #endringDato');
+});
+
+$(document).ready(function(){
+  initTimepicker('#regFraTidspunkt, #regTilTidspunkt, #endringFraTidspunkt, #endringTilTidspunkt');
+});
